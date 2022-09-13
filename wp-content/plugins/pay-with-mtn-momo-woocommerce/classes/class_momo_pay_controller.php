38a0f5929299ae398bd61fb1d173e02891b5422c
maksimmos16 <maksimmos1629@gmail.com>
2022-09-13 11:14:24 +0400

    second commit

---
+++ wp-content/plugins/pay-with-mtn-momo-woocommerce/classes/class_momo_pay_controller.php
@@ -0,0 +1,35 @@
+<?php
+class WC_MomoPay_Controller {
+
+	protected static function getAnswerObject($res) {
+		$o = new stdClass();
+		$o->status = (isset($res->status)) ? $res->status : '';
+		$o->message = (isset($res->message)) ? $res->message : '';
+		$o->data = (isset($res->data)) ? @$res->data : '';
+		$o->code = (isset($res->code)) ? @$res->code : 200;
+		
+		return json_encode($o);	
+	}
+	
+	public static function processData($data) {
+		if(!$data) return 'Input data error!';
+		
+		$p = new WC_MomoPay_Gateway();
+		
+		$res = '';
+		switch(@$data['mode']) {
+			case 'check_payment':
+				$payment_id = (@$data['payment_id']) ? $data['payment_id'] : "x";
+				$res = $p->getPaymentStatus($payment_id);
+				if($res->status) {
+					$p->updateTransaction($payment_id, $res);
+				}
+				break;
+			default:
+				break;
+		};
+	
+		return self::getAnswerObject($res);
+	}	
+}	
+?>
